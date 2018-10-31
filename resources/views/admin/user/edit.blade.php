0548e476c09976e28271bed95c05c99a8a2c6666
QSHANSHAN <15093859812@163.com>
2018-10-31 11:51:15 +0800

    用户增删改查做完

---
+++ resources/views/admin/user/edit.blade.php
@@ -0,0 +1,112 @@
+@extends('admin.clone_index.clone')
+@section('content')
+	<!-- 显示验证错误信息 -->
+	@if (count($errors) > 0)
+    <div class="mws-form-message error">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+	@endif
+
+	<div class="mws-panel grid_8">
+	<div class="mws-panel-header">
+    	<span><i class="icon-magic"></i>{{ $title}}</span>
+    </div>
+    <div class="mws-panel-body no-padding">
+        
+        <form class="mws-form wizard-form wizard-form-horizontal" id="mws-wizard-form" action="/admin/user/{{$date['id']}}" method='post'>
+            {{ csrf_field()}}
+            {{ method_field('PUT')}}
+            <fieldset class="mws-form-inline" id="step-1" style="display: block;" data-wzd-id="wzd_1cr022tv31sejegk82h_0">
+                <legend class="wizard-label" style="display: none;"><i class="icol-accept"></i> Member Profile</legend>
+                <div class="mws-form-row">
+                    <label class="mws-form-label">用户名 <span class="required">*</span></label>
+                    <div class="mws-form-item">
+                        <input name='username' class="required large" type="text" value='{{ $date["username"]}}'>
+                    </div>
+                </div>
+                <div class="mws-form-row">
+                    <label class="mws-form-label">密码 <span class="required">*</span></label>
+                    <div class="mws-form-item">
+                        <input name="password" class="required email large" type="password">
+                    </div>
+                </div>
+                <div class="mws-form-row">
+                    <label class="mws-form-label">确认密码 <span class="required">*</span></label>
+                    <div class="mws-form-item">
+                        <input name="repassword" class="required email large" type="password">
+                    </div>
+                </div>
+                 <div class="mws-form-row">
+                    <label class="mws-form-label">邮箱<span class="required">*</span></label>
+                    <div class="mws-form-item">
+                        <input name="email" class="required email large" type="text" value='{{ $date['email']}}'>
+                    </div>
+                </div>
+                 <div class="mws-form-row">
+                    <label class="mws-form-label">手机号码 <span class="required">*</span></label>
+                    <div class="mws-form-item">
+                        <input name="phone" class="required email large" type="text" value='{{ $date['phone']}}'>
+                    </div>
+                </div>
+                 
+                <div class="mws-form-row">
+                    <label class="mws-form-label">管理权限 <span class="required">*</span></label>
+                     <select name='status'>
+                           		<option value='1' @if($date['status'] ==0) selected @endif >管理员  </option>
+                           		<option value='2' @if($date['status'] ==1) selected @endif>普通用户</option>
+                      </select>
+                    <div class="mws-form-item">
+                       
+                        <label class="error plain" style="display:none" for="gender" generated="true"></label>
+                    </div>
+                </div>
+             
+            </fieldset>
+            
+            <fieldset class="mws-form-inline" id="step-2" style="display: none;" data-wzd-id="wzd_1cr022tv31sejegk82h_1">
+                <legend class="wizard-label" style="display: none;"><i class="icol-delivery"></i> Membership Type</legend>
+                <div class="mws-form-row">
+                    <label class="mws-form-label">Membership Plan <span class="required">*</span></label>
+                    <div class="mws-form-item">
+                        <select class="required large">
+                        	<option>Free</option>
+                        	<option>Standard</option>
+                        	<option>Premium</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="mws-form-row">
+                    <label class="mws-form-label">Subscription Period <span class="required">*</span></label>
+                    <div class="mws-form-item">
+                        <select class="required large">
+                        	<option>One Month</option>
+                        	<option>Six Months</option>
+                        	<option>Twelve Months</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="mws-form-row">
+                    <label class="mws-form-label">Payment Method <span class="required">*</span></label>
+                    <div class="mws-form-item">
+                        <ul class="mws-form-list">
+                            <li><input name="pm" class="required" id="cc" type="radio"> <label for="cc">Credit Card</label></li>
+                            <li><input name="pm" id="pp" type="radio"> <label for="pp">PayPal</label></li>
+                        </ul>
+                        <label class="error plain" style="display:none" for="pm" generated="true"></label>
+                    </div>
+                </div>
+            </fieldset>
+            
+        
+        <div class="mws-button-row">
+        	<input type="submit" class='btn btn-info' value="提交">
+        </div>
+    </form>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,79 @@
+@extends('admin.clone_index.clone');
+
+@section('content')
+ <div class="mws-panel grid_8">
+                <div class="mws-panel-header">
+                    <span><i class="icon-table"></i> 用户列表</span>
+                </div>
+                <div class="mws-panel-body no-padding">
+                    <div class="dataTables_wrapper" id="DataTables_Table_1_wrapper" role="grid">
+
+                        <form action='/admin/user' method='get'>
+                             <div class="dataTables_length" id="DataTables_Table_1_length">
+                                <label>显示<select name="showCount"  size="1">
+                                    <option selected="selected" value="5">5</option>
+                                  
+                                </select> 条</label>
+                            </div>
+                            <div class="dataTables_filter" id="DataTables_Table_1_filter">
+                            <label>关键字:
+                                 <input name='search' aria-controls="DataTables_Table_1" type="text">
+                             </label>
+                                <input type="submit" value='搜索' class='btn btn-info'>
+                            </div>
+
+                        </form>
+                       
+                        <table class="mws-datatable-fn mws-table dataTable" id="DataTables_Table_1" aria-describedby="DataTables_Table_1_info">
+                        <thead>
+                           <tr>
+                               <th>ID</th>
+                               <th>用户名</th>
+                               <th>手机号</th>
+                               <th>邮箱</th>
+                               <th>注册时间</th>
+                               <th>修改时间</th>
+                               <th>权限</th>
+                               <th>操作</th>
+                           </tr>
+                        </thead>
+                        
+                    <tbody role="alert" aria-live="polite" aria-relevant="all">
+                        
+                                @foreach($user as $k=>$v)
+                            <tr class="odd">
+                                <td class="">{{ $v['id']}}</td>
+                                <td class="">{{ $v['username']}}</td>
+                                <td>{{ $v['phone']}}</td>
+                                <td>{{ $v['email']}}</td>
+                                <td>{{ $v['created_at']}}</td>
+                                <td>{{ $v['updated_at']}}</td>
+                                <td>{{ $v['status'] == 1 ? '普通用户' : '管理员'}}</td>
+                              
+                                <td> 
+                                     <a href="/admin/user/{{ $v['id']}}/edit" class='btn btn-warning'>修改</a>                        
+                                    <form  style="display:inline-block" action='/admin/user/{{ $v["id"]}}' method='post'>
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        <input type="submit" value="删除" class='btn btn-danger'>
+                                        
+                                    </form>
+                                 
+                                </td>
+                            </tr>
+                              @endforeach
+                        </tbody>
+                    </table>
+                    <div class="dataTables_info" id="DataTables_Table_1_info">Showing 1 to 10 of 57 entries
+                        
+                    </div>
+                      <div id='page_page'>
+                            {!! $user->render() !!}
+                    
+                        </div>
+                  
+                </div>
+                
+                </div>
+            </div>
+@endsection
